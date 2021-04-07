23cda8a31040fadfcf26137d3ea7defe9892fbc4
Björn Hafstad <bjornhafstad@Bjorns-MacBook-Pro.local>
2021-04-07 14:19:14 +0200

    first commit of the project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+// gammal typing av funktioner
+// function App() {
+//   return (
+//     <div>
+//       <h1>Test</h1>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+// nya sättet att skriva funktioner - kräver måsklamrar när app importeras till index.js
+export const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Test!</h1>
     </div>
-  );
+  )
 }
-
-export default App;
